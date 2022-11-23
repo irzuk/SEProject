cb139ca827333dad4c00ed60a842e43abba360fb
Kristina Trofimova <aktrofimova@mail.ru>
2022-11-23 14:13:02 +0300

    data struts added

---
+++ src/main/java/Ingridient.java
@@ -0,0 +1,5 @@
+public class Ingridient {
+    public String name;
+    public Integer amount;
+    public String unit;
+}
---
+++ src/main/java/Recipe.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Recipe {
+    public String name;
+    public List<Ingridient> ingridients;
+    public String instruction;
+}
