cf08ec124e870dfb603e9383d5ad9de6f4eb0dc0
Kristina Trofimova <aktrofimova@mail.ru>
2022-11-30 14:11:18 +0300

    cockbook refactor

--- src/main/java/CookbookFuncs.java
+++ src/main/java/CookbookFuncs.java
@@ -2,16 +2,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CookbookFuncs {
-    public List<Recipe> SearchIngridient(List<Recipe> recs, Ingridient ing) {
+    public List<Recipe> SearchIngridients(List<Recipe> recs, List<Ingridient> ings) {
         List<Recipe> final_recs = new ArrayList<>();
         for (Recipe rec : recs) {
-            for (Ingridient i : rec.ingridients) {
-                if (i.equals(ing)) {
-                    final_recs.add(rec);
+            boolean flag = true;
+            for (Ingridient i : ings) {
+                if (!hasIngridient(rec, i)) {
+                    flag = false;
                     break;
                 }
             }
+            if (flag)
+                final_recs.add(rec);
         }
         return final_recs;
+    }
+
+    private Boolean hasIngridient(Recipe rec, Ingridient ing) {
+        for (Ingridient i : rec.ingridients) {
+            if (i.equals(ing)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
